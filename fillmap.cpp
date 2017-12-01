e0a0ada2b03cce2a97bfffa467d5fbd2d3d051bc
Hugo Lefeuvre <hle@debian.org>
2017-12-01 12:03:21 +0100

    Make code multiplatform and fix various issues
    * Use macros to determine platform (arduboy/pure arduino/linux)
    * Avoid code duplication in get_at_xy() and set_at_xy()
    * Add some (int) casts needed on linux
    * Use unsigned char in the chunk_t type
    * Use ints instead of shorts

--- fillmap.cpp
+++ fillmap.cpp
@@ -1,38 +1,74 @@
 #include "fillmap.h"
 #include <stdlib.h>
 #include <string.h>
-#include <Arduboy2.h>
 
+#if defined(ARDUBOY_PLATFORM)
+    #include <Arduboy2.h>
+#elif defined(ARDUINO_PLATFORM)
+    #include <Arduino.h>
+#else
+    #include <algorithm>
+
+    using std::min;
+
+    int random(int min, int max) {
+        if(min >= max) {
+            return min;
+        }
+        return random() % (max - min) + min;
+    }
+#endif
+
+static const int chunk_size_in_bits = sizeof(chunk_t) * 8;
+
+bool is_valid_coordinate(map_t* map, int x, int y) {
+    if (x < 0 || x > map->width || y < 0 || y > map->height) {
+        return false;
+    }
+    return true;
+}
+
+int get_chunk_offset(map_t* map, int x, int y) {
+    return y * map->chunked_width + roundDown(x, chunk_size_in_bits) / chunk_size_in_bits;
+}
+
+int get_bit_offset(map_t* map, int x) {
+    return x % chunk_size_in_bits;
+}
 
 int get_at_xy(map_t* map, int x, int y) {
-    int struct_offset = y * map->chunked_width + roundDown(x, sizeof(char) * 8) / (sizeof(char) * 8);
-    int bit_offset = x % (sizeof(char) * 8);
+    if (!is_valid_coordinate(map, x, y)) {
+        return -1;
+    }
+
+    int chunk_offset = get_chunk_offset(map, x, y);
+    int bit_offset   = get_bit_offset(map, x);
 
     int element;
     switch (bit_offset) {
         case 0:
-            element = map->chunks[struct_offset].e1;
+            element = map->chunks[chunk_offset].e1;
             break;
         case 1:
-            element = map->chunks[struct_offset].e2;
+            element = map->chunks[chunk_offset].e2;
             break;
         case 2:
-            element = map->chunks[struct_offset].e3;
+            element = map->chunks[chunk_offset].e3;
             break;
         case 3:
-            element = map->chunks[struct_offset].e4;
+            element = map->chunks[chunk_offset].e4;
             break;
         case 4:
-            element = map->chunks[struct_offset].e5;
+            element = map->chunks[chunk_offset].e5;
             break;
         case 5:
-            element = map->chunks[struct_offset].e6;
+            element = map->chunks[chunk_offset].e6;
             break;
         case 6:
-            element = map->chunks[struct_offset].e7;
+            element = map->chunks[chunk_offset].e7;
             break;
         case 7:
-            element = map->chunks[struct_offset].e8;
+            element = map->chunks[chunk_offset].e8;
             break;
         default:
             element = -1;
@@ -42,35 +78,43 @@ int get_at_xy(map_t* map, int x, int y) {
 }
 
 int set_at_xy(map_t* map, int x, int y, int to_set) {
-    int struct_offset = y * map->chunked_width + roundDown(x, sizeof(char) * 8) / (sizeof(char) * 8);
-    int bit_offset = x % (sizeof(char) * 8);
+    if (!is_valid_coordinate(map, x, y)) {
+        return -1;
+    }
+
+    int chunk_offset = get_chunk_offset(map, x, y);
+    int bit_offset   = get_bit_offset(map, x);
 
     switch (bit_offset) {
         case 0:
-            map->chunks[struct_offset].e1 = to_set;
+            map->chunks[chunk_offset].e1 = to_set;
             break;
         case 1:
-            map->chunks[struct_offset].e2 = to_set;
+            map->chunks[chunk_offset].e2 = to_set;
             break;
         case 2:
-            map->chunks[struct_offset].e3 = to_set;
+            map->chunks[chunk_offset].e3 = to_set;
             break;
         case 3:
-            map->chunks[struct_offset].e4 = to_set;
+            map->chunks[chunk_offset].e4 = to_set;
             break;
         case 4:
-            map->chunks[struct_offset].e5 = to_set;
+            map->chunks[chunk_offset].e5 = to_set;
             break;
         case 5:
-            map->chunks[struct_offset].e6 = to_set;
+            map->chunks[chunk_offset].e6 = to_set;
             break;
         case 6:
-            map->chunks[struct_offset].e7 = to_set;
+            map->chunks[chunk_offset].e7 = to_set;
             break;
         case 7:
-            map->chunks[struct_offset].e8 = to_set;
+            map->chunks[chunk_offset].e8 = to_set;
             break;
+        default:
+            return -1;
     }
+
+    return 0;
 }
 
 unsigned int roundUp(unsigned int value, unsigned int roundTo)
@@ -94,15 +138,12 @@ int fill_map(map_t* map, float min_filling, float max_room_size) {
 
     int map_size = map->width * map->height;
 
-    // Initialize map
-    memset(map->chunks, CHAR_EMPTY, map_size * sizeof(char));
-
     coordinate point;
 
     // Choose initial point at random, make sure it fulfills is_suitable_initial_point
     do {
-        point = (coordinate) { .x = (unsigned short) random(0, map->width),
-                               .y = (unsigned short) random(0, map->height)};
+        point = (coordinate) { .x = (int) random(0, map->width),
+                               .y = (int) random(0, map->height)};
     } while (!is_suitable_initial_point(map, point));
 
     // Generation loop
@@ -131,11 +172,11 @@ int is_suitable_initial_point(map_t* map, coordinate initial_point) {
 }
 
 void compute_room_dimensions(map_t* map, coordinate* point, int *height, int *width, float max_room_size_factor) {
-    int max_height = min(map->height - point->y, point->y);
-    max_height = min(max_height, map->height * max_room_size_factor * 0.5f);
+    int max_height = min(map->height - point->y, (int) point->y);
+    max_height = min(max_height, (int) (map->height * max_room_size_factor * 0.5f));
 
-    int max_width = min(map->width - point->x, point->x);
-    max_width = min(max_width, map->width * max_room_size_factor * 0.5f);
+    int max_width = min(map->width - point->x, (int) point->x);
+    max_width = min(max_width, (int) (map->width * max_room_size_factor * 0.5f));
 
     *height = random(1, max_height);
     *width = random(1, max_width);
--- fillmap.h
+++ fillmap.h
@@ -3,19 +3,22 @@
 #define CHAR_ROOM 1
 #define CHAR_EMPTY 0
 
+//#define ARDUINO_PLATFORM
+//#define ARDUBOY_PLATFORM
+
 typedef struct {
-  unsigned short x, y;
+  int x, y;
 } coordinate;
 
 typedef struct {
-  char e1 : 1;
-  char e2 : 1;
-  char e3 : 1;
-  char e4 : 1;
-  char e5 : 1;
-  char e6 : 1;
-  char e7 : 1;
-  char e8 : 1;
+  unsigned char e1 : 1;
+  unsigned char e2 : 1;
+  unsigned char e3 : 1;
+  unsigned char e4 : 1;
+  unsigned char e5 : 1;
+  unsigned char e6 : 1;
+  unsigned char e7 : 1;
+  unsigned char e8 : 1;
 } chunk_t;
 
 typedef struct {
@@ -34,6 +37,13 @@ int generate_elliptic_room(map_t* map, coordinate* point, float max_room_size_fa
 void compute_room_dimensions(map_t* map, coordinate* point, int *height, int *width, float max_room_size_factor);
 
 int get_at_xy(map_t* map, int x, int y);
+
+int set_at_xy(map_t* map, int x, int y, int to_set);
+
+bool is_valid_coordinate(map_t* map, int x, int y);
+
+int get_chunk_offset(map_t* map, int x, int y);
+int get_bit_offset(map_t* map, int x);
 
 unsigned int roundUp(unsigned int value, unsigned int roundTo);
 unsigned int roundDown(unsigned int value, unsigned int roundTo);
